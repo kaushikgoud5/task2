fc523af2399bfbdaa400119383e52def284efa63
Kaushik Nakka <kaushik.n@Technovert.com>
2024-02-08 22:28:03 +0530

    second-commit

--- script.js
+++ script.js
@@ -1,10 +1,466 @@
-import dataEmployess from "./data1.json" assert { type: 'json' };
-import deptData from  "./data2.json" assert {type:'json'};
+let dataEmployess = [
+  {
+    USER: "etrevain0",
+    LOCATION: "Jiachuan",
+    DEPARTMENT: "Sales",
+    ROLE: "Construction Manager",
+    EMPNO: "01HNYVWWWH6Z4M4P914RNPBM7A",
+    STATUS: false,
+    JOINDT: "8/14/2023",
+  },
+  {
+    USER: "msparham1",
+    LOCATION: "Odzi",
+    DEPARTMENT: "Marketing",
+    ROLE: "Electrician",
+    EMPNO: "01HNYVWWWMVE6CJJ7252JG3E9X",
+    STATUS: false,
+    JOINDT: "11/28/2023",
+  },
+  {
+    USER: "overegan2",
+    LOCATION: "Severnyy",
+    DEPARTMENT: "Business Development",
+    ROLE: "Construction Manager",
+    EMPNO: "01HNYVWWWP7BMYKYVASRG461EE",
+    STATUS: true,
+    JOINDT: "5/25/2023",
+  },
+  {
+    USER: "jlilleyman3",
+    LOCATION: "Fuyo",
+    DEPARTMENT: "Business Development",
+    ROLE: "Subcontractor",
+    EMPNO: "01HNYVWWWR15XTNB18ZH4Q5EDP",
+    STATUS: true,
+    JOINDT: "2/25/2023",
+  },
+  {
+    USER: "bdesbrow4",
+    LOCATION: "Smokvica",
+    DEPARTMENT: "Accounting",
+    ROLE: "Estimator",
+    EMPNO: "01HNYVWWWSBK0812G1D4YQN0JW",
+    STATUS: true,
+    JOINDT: "12/14/2023",
+  },
+  {
+    USER: "edohrmann5",
+    LOCATION: "Selydove",
+    DEPARTMENT: "Sales",
+    ROLE: "Surveyor",
+    EMPNO: "01HNYVWWWVSV3ZX95MSQD47M7R",
+    STATUS: false,
+    JOINDT: "11/15/2023",
+  },
+  {
+    USER: "sjoseff6",
+    LOCATION: "Amparafaravola",
+    DEPARTMENT: "Accounting",
+    ROLE: "Electrician",
+    EMPNO: "01HNYVWWWVSMRVR998Z20F1NBW",
+    STATUS: true,
+    JOINDT: "1/31/2024",
+  },
+  {
+    USER: "tdelepine7",
+    LOCATION: "Szamotuły",
+    DEPARTMENT: "Sales",
+    ROLE: "Subcontractor",
+    EMPNO: "01HNYVWWWWAXY0J8KNFD56A3DV",
+    STATUS: true,
+    JOINDT: "9/25/2023",
+  },
+  {
+    USER: "adelve8",
+    LOCATION: "Solnechnyy",
+    DEPARTMENT: "Sales",
+    ROLE: "Project Manager",
+    EMPNO: "01HNYVWWWXK7GZ95N84MM852F2",
+    STATUS: true,
+    JOINDT: "11/28/2023",
+  },
+  {
+    USER: "etumielli9",
+    LOCATION: "Estela",
+    DEPARTMENT: "Research and Development",
+    ROLE: "Estimator",
+    EMPNO: "01HNYVWWWYQZME2EYT84MWXJHM",
+    STATUS: true,
+    JOINDT: "8/25/2023",
+  },
+  {
+    USER: "dlaprea",
+    LOCATION: "Ruše",
+    DEPARTMENT: "Accounting",
+    ROLE: "Project Manager",
+    EMPNO: "01HNYVWWWZDATRRWJ1SNFXXGAZ",
+    STATUS: false,
+    JOINDT: "6/4/2023",
+  },
+  {
+    USER: "ssmithersb",
+    LOCATION: "Apeldoorn",
+    DEPARTMENT: "Accounting",
+    ROLE: "Project Manager",
+    EMPNO: "01HNYVWWX0K730TAAE8B2CZEA3",
+    STATUS: false,
+    JOINDT: "1/16/2024",
+  },
+  {
+    USER: "pmegaineyc",
+    LOCATION: "Cairima",
+    DEPARTMENT: "Training",
+    ROLE: "Architect",
+    EMPNO: "01HNYVWWX192CZ17CP93N4QKV5",
+    STATUS: false,
+    JOINDT: "8/23/2023",
+  },
+  {
+    USER: "hlefleyd",
+    LOCATION: "Baja Mar",
+    DEPARTMENT: "Product Management",
+    ROLE: "Construction Manager",
+    EMPNO: "01HNYVWWX2X9GSY3KHABZGS5YH",
+    STATUS: false,
+    JOINDT: "9/20/2023",
+  },
+  {
+    USER: "tlargee",
+    LOCATION: "Lojigawaran",
+    DEPARTMENT: "Services",
+    ROLE: "Supervisor",
+    EMPNO: "01HNYVWWX4ZX48NMTESHMS4BA0",
+    STATUS: false,
+    JOINDT: "6/6/2023",
+  },
+  {
+    USER: "elecordierf",
+    LOCATION: "Huolongmen",
+    DEPARTMENT: "Research and Development",
+    ROLE: "Construction Worker",
+    EMPNO: "01HNYVWWX5V4VDRM1V3N7BGARE",
+    STATUS: false,
+    JOINDT: "8/29/2023",
+  },
+  {
+    USER: "bsondlandg",
+    LOCATION: "Dafundo",
+    DEPARTMENT: "Research and Development",
+    ROLE: "Architect",
+    EMPNO: "01HNYVWWX6QDGW4KFJVXR6A96R",
+    STATUS: true,
+    JOINDT: "6/1/2023",
+  },
+  {
+    USER: "mdaykinh",
+    LOCATION: "London",
+    DEPARTMENT: "Training",
+    ROLE: "Estimator",
+    EMPNO: "01HNYVWWX8A8TFYTBSPREJRW4G",
+    STATUS: false,
+    JOINDT: "9/4/2023",
+  },
+  {
+    USER: "ccockilli",
+    LOCATION: "Wierzchowo",
+    DEPARTMENT: "Sales",
+    ROLE: "Construction Foreman",
+    EMPNO: "01HNYVWWXAQMBJMDFM35JEH7MM",
+    STATUS: false,
+    JOINDT: "7/9/2023",
+  },
+  {
+    USER: "tstainsbyj",
+    LOCATION: "Būsh",
+    DEPARTMENT: "Sales",
+    ROLE: "Estimator",
+    EMPNO: "01HNYVWWXBC1TDK4GXM3GZM6RZ",
+    STATUS: false,
+    JOINDT: "2/18/2023",
+  },
+  {
+    USER: "ccastangiak",
+    LOCATION: "Tigarunggu",
+    DEPARTMENT: "Training",
+    ROLE: "Engineer",
+    EMPNO: "01HNYVWWXDQHEDMHVHSSEY8TGS",
+    STATUS: false,
+    JOINDT: "10/16/2023",
+  },
+  {
+    USER: "cpeascodl",
+    LOCATION: "Hlybokaye",
+    DEPARTMENT: "Marketing",
+    ROLE: "Electrician",
+    EMPNO: "01HNYVWWXFHY18JR6SASG2TQP0",
+    STATUS: false,
+    JOINDT: "1/7/2024",
+  },
+  {
+    USER: "eyekelm",
+    LOCATION: "Chekmagush",
+    DEPARTMENT: "Training",
+    ROLE: "Engineer",
+    EMPNO: "01HNYVWWXGZRBG535FHCBGMTW1",
+    STATUS: true,
+    JOINDT: "1/1/2024",
+  },
+  {
+    USER: "hbushnelln",
+    LOCATION: "Tharyarwady",
+    DEPARTMENT: "Engineering",
+    ROLE: "Subcontractor",
+    EMPNO: "01HNYVWWXHW9SV9FRN258TKP8D",
+    STATUS: true,
+    JOINDT: "9/14/2023",
+  },
+  {
+    USER: "mohengertyo",
+    LOCATION: "Viso",
+    DEPARTMENT: "Research and Development",
+    ROLE: "Supervisor",
+    EMPNO: "01HNYVWWXJJC6TAW1NB028D02Y",
+    STATUS: false,
+    JOINDT: "8/25/2023",
+  },
+  {
+    USER: "lfrancap",
+    LOCATION: "Ochota",
+    DEPARTMENT: "Research and Development",
+    ROLE: "Architect",
+    EMPNO: "01HNYVWWXJCSNF6PTCMEN7ME7C",
+    STATUS: false,
+    JOINDT: "5/16/2023",
+  },
+  {
+    USER: "psoutheyq",
+    LOCATION: "Petkovci",
+    DEPARTMENT: "Marketing",
+    ROLE: "Surveyor",
+    EMPNO: "01HNYVWWXK11ZRR68S1HFE1TDQ",
+    STATUS: true,
+    JOINDT: "10/17/2023",
+  },
+  {
+    USER: "rkimblinr",
+    LOCATION: "Yamoto",
+    DEPARTMENT: "Sales",
+    ROLE: "Estimator",
+    EMPNO: "01HNYVWWXMN6METRX2TRDTB82D",
+    STATUS: true,
+    JOINDT: "10/1/2023",
+  },
+  {
+    USER: "mborrows",
+    LOCATION: "Xinshi",
+    DEPARTMENT: "Sales",
+    ROLE: "Electrician",
+    EMPNO: "01HNYVWWXN9JMFCKR7FA3030S8",
+    STATUS: true,
+    JOINDT: "3/2/2023",
+  },
+  {
+    USER: "bbasilonet",
+    LOCATION: "Pacobamba",
+    DEPARTMENT: "Human Resources",
+    ROLE: "Project Manager",
+    EMPNO: "01HNYVWWXPS79XEMHSHSWAF8W8",
+    STATUS: false,
+    JOINDT: "4/10/2023",
+  },
+  {
+    USER: "fferrimanu",
+    LOCATION: "Taen Tengah",
+    DEPARTMENT: "Services",
+    ROLE: "Supervisor",
+    EMPNO: "01HNYVWWXQ58EZNTN2PXE07PZ2",
+    STATUS: true,
+    JOINDT: "3/20/2023",
+  },
+  {
+    USER: "bdawltreyv",
+    LOCATION: "Bethlehem",
+    DEPARTMENT: "Engineering",
+    ROLE: "Electrician",
+    EMPNO: "01HNYVWWXR7AHEF62HDT9GYPDB",
+    STATUS: false,
+    JOINDT: "3/29/2023",
+  },
+  {
+    USER: "mjozefowiczw",
+    LOCATION: "Dallas",
+    DEPARTMENT: "Human Resources",
+    ROLE: "Engineer",
+    EMPNO: "01HNYVWWXSJ9WJ4MS214KW6AR1",
+    STATUS: false,
+    JOINDT: "11/8/2023",
+  },
+  {
+    USER: "jellyattx",
+    LOCATION: "Canchaque",
+    DEPARTMENT: "Accounting",
+    ROLE: "Supervisor",
+    EMPNO: "01HNYVWWXTV0E8QC127Y65SESX",
+    STATUS: true,
+    JOINDT: "9/11/2023",
+  },
+  {
+    USER: "ctotterdelly",
+    LOCATION: "Włoszakowice",
+    DEPARTMENT: "Legal",
+    ROLE: "Subcontractor",
+    EMPNO: "01HNYVWWXTPXGXZZ58T9TC1DZC",
+    STATUS: true,
+    JOINDT: "12/16/2023",
+  },
+  {
+    USER: "aleynhamz",
+    LOCATION: "Klek",
+    DEPARTMENT: "Engineering",
+    ROLE: "Subcontractor",
+    EMPNO: "01HNYVWWXV3645DTKP8BX4RQ2R",
+    STATUS: false,
+    JOINDT: "11/13/2023",
+  },
+  {
+    USER: "ksylvester10",
+    LOCATION: "Obudovac",
+    DEPARTMENT: "Engineering",
+    ROLE: "Construction Expeditor",
+    EMPNO: "01HNYVWWXX40AC61B6S91ETRCZ",
+    STATUS: false,
+    JOINDT: "9/23/2023",
+  },
+  {
+    USER: "itooting11",
+    LOCATION: "Xigou",
+    DEPARTMENT: "Support",
+    ROLE: "Construction Worker",
+    EMPNO: "01HNYVWWXZBYMWZPQCWC4GFMAF",
+    STATUS: true,
+    JOINDT: "8/14/2023",
+  },
+  {
+    USER: "kpottage12",
+    LOCATION: "Uddiawan",
+    DEPARTMENT: "Engineering",
+    ROLE: "Engineer",
+    EMPNO: "01HNYVWWY04TSQDJ81P30CGDMD",
+    STATUS: false,
+    JOINDT: "5/23/2023",
+  },
+  {
+    USER: "estandfield13",
+    LOCATION: "Leśna Podlaska",
+    DEPARTMENT: "Product Management",
+    ROLE: "Engineer",
+    EMPNO: "01HNYVWWY1486X0PJJBKJ4Q2T6",
+    STATUS: true,
+    JOINDT: "8/1/2023",
+  },
+  {
+    USER: "gmeeus14",
+    LOCATION: "Embu Guaçu",
+    DEPARTMENT: "Legal",
+    ROLE: "Construction Foreman",
+    EMPNO: "01HNYVWWY2MZYD9NE5MEQ9A1QE",
+    STATUS: false,
+    JOINDT: "8/9/2023",
+  },
+  {
+    USER: "ywolters15",
+    LOCATION: "Vesoul",
+    DEPARTMENT: "Legal",
+    ROLE: "Architect",
+    EMPNO: "01HNYVWWY2B5C167ATTA4NZGYW",
+    STATUS: false,
+    JOINDT: "3/24/2023",
+  },
+  {
+    USER: "ajosovitz16",
+    LOCATION: "Gurlan",
+    DEPARTMENT: "Business Development",
+    ROLE: "Construction Expeditor",
+    EMPNO: "01HNYVWWY39J5VTXN7J52Y0Y8W",
+    STATUS: false,
+    JOINDT: "8/19/2023",
+  },
+  {
+    USER: "abletsor17",
+    LOCATION: "Pantenan",
+    DEPARTMENT: "Research and Development",
+    ROLE: "Construction Foreman",
+    EMPNO: "01HNYVWWY4MMW3KSGX9XPDGPQQ",
+    STATUS: false,
+    JOINDT: "4/30/2023",
+  },
+  {
+    USER: "acyphus18",
+    LOCATION: "Aramecina",
+    DEPARTMENT: "Marketing",
+    ROLE: "Subcontractor",
+    EMPNO: "01HNYVWWY5SB1ZP2QMDRK5JVC0",
+    STATUS: false,
+    JOINDT: "12/18/2023",
+  },
+  {
+    USER: "ekingsnode19",
+    LOCATION: "Eybens",
+    DEPARTMENT: "Engineering",
+    ROLE: "Construction Foreman",
+    EMPNO: "01HNYVWWY6R4HMJMWYPX2C34ZT",
+    STATUS: true,
+    JOINDT: "7/26/2023",
+  },
+  {
+    USER: "nandriveaux1a",
+    LOCATION: "Pirapozinho",
+    DEPARTMENT: "Engineering",
+    ROLE: "Construction Foreman",
+    EMPNO: "01HNYVWWY7F083H6WNMQ44R5X9",
+    STATUS: true,
+    JOINDT: "2/22/2023",
+  },
+  {
+    USER: "gbims1b",
+    LOCATION: "Antwerpen",
+    DEPARTMENT: "Legal",
+    ROLE: "Construction Foreman",
+    EMPNO: "01HNYVWWY7QMWDMPSM2SC1TX14",
+    STATUS: false,
+    JOINDT: "12/31/2023",
+  },
+  {
+    USER: "dcassam1c",
+    LOCATION: "Porto Ferreira",
+    DEPARTMENT: "Research and Development",
+    ROLE: "Architect",
+    EMPNO: "01HNYVWWY8YBBXA0W8A673S0MQ",
+    STATUS: true,
+    JOINDT: "6/19/2023",
+  },
+  {
+    USER: "ldugan1d",
+    LOCATION: "Seleuš",
+    DEPARTMENT: "Services",
+    ROLE: "Supervisor",
+    EMPNO: "01HNYVWWYAFQ8MDBZ16H656PDE",
+    STATUS: false,
+    JOINDT: "3/29/2023",
+  },
+];
+// y=[]
+// fetch("./data1.json").then((response)=>{
+//   return response.json();
+// }).then((x)=>{
+//   tableDisplay(x)
+// })
+// console.log(y);
+// console.log((dataEmployess))
 
-function showDropDownEmployees() {
-  document.getElementById("list-items-drop-down").classList.toggle("show");
-  document.getElementById("arrow-right").classList.toggle("drop-arrow");
-}
+
+
 function hideNavBar() {
   document.getElementById("side-nav-bar").classList.toggle("hide-navbar");
   document.getElementById("close-btn").classList.toggle("rotate-arrow");
@@ -24,22 +480,19 @@ function tableDisplay(dataEmployess) {
     rows_tr += ` <tr id="${index}">
    <td><input type="checkbox"  class="inp-check" id="check-${index}" onclick="handleSingleCheckbox(this,${index})" /></td>
    <td>
-     <div class="table-user-info" id="user-profile">
+     <div class="table-user-info d-flex" id="user-profile">
          <img src="./assets/user-table.jpg" alt="">
        <div class="name-email" >
-         <span>${ele.USER.toUpperCase()}</span><br /><span class="e-mail">${ele.USER.slice(
-      0,
-      5
-    )}@tezo.com</span>
+         <span>${ele.USER.toUpperCase()}</span><br /><span class="e-mail">@tezo.com</span>
        </div>
      </div>
    </td>
    <td>${ele.LOCATION}</td>
    <td>${ele.DEPARTMENT}</td>
    <td>${ele.ROLE}</td>
-   <td>${ele.EMPNO.slice(0, 8)}</td>
+   <td>${ele.EMPNO.slice(0,5)}</td>
    <td><span>${ele.STATUS === true ? "Active" : "Inactive"}</span></td>
-   <td>${ele.JOINDT.slice(0, 10)}</td>
+   <td>${ele.JOINDT}</td>
    <td>...</td>
    </tr>`;
   });
@@ -89,7 +542,6 @@ function handleSortingUser() {
       return 0;
     });
 
-    console.log(sortedData);
     tableDisplay(sortedData);
   } else {
     asc = true;
@@ -104,7 +556,6 @@ function handleSortingUser() {
       return 0;
     });
 
-    console.log(sortedData);
     tableDisplay(sortedData);
   }
 }
@@ -122,7 +573,7 @@ function handleSortingLoc() {
       return 0;
     });
 
-    console.log(sortedData);
+
     tableDisplay(sortedData);
   } else {
     asc = true;
@@ -137,7 +588,6 @@ function handleSortingLoc() {
       return 0;
     });
 
-    console.log(sortedData);
     tableDisplay(sortedData);
   }
 }
@@ -154,8 +604,6 @@ function handleSortingDep() {
       }
       return 0;
     });
-
-    console.log(sortedData);
     tableDisplay(sortedData);
   } else {
     asc = true;
@@ -170,7 +618,8 @@ function handleSortingDep() {
       return 0;
     });
 
-    console.log(sortedData);
+   
+  
     tableDisplay(sortedData);
   }
 }
@@ -187,8 +636,6 @@ function handleSortingRole() {
       }
       return 0;
     });
-
-    console.log(sortedData);
     tableDisplay(sortedData);
   } else {
     asc = true;
@@ -203,7 +650,6 @@ function handleSortingRole() {
       return 0;
     });
 
-    console.log(sortedData);
     tableDisplay(sortedData);
   }
 }
@@ -221,7 +667,7 @@ function handleSortingEmp() {
       return 0;
     });
 
-    console.log(sortedData);
+
     tableDisplay(sortedData);
   } else {
     asc = true;
@@ -236,7 +682,6 @@ function handleSortingEmp() {
       return 0;
     });
 
-    console.log(sortedData);
     tableDisplay(sortedData);
   }
 }
@@ -254,7 +699,7 @@ function handleSortingStatus() {
       return 0;
     });
 
-    console.log(sortedData);
+
     tableDisplay(sortedData);
   } else {
     asc = true;
@@ -268,8 +713,6 @@ function handleSortingStatus() {
       }
       return 0;
     });
-
-    console.log(sortedData);
     tableDisplay(sortedData);
   }
 }
@@ -286,8 +729,6 @@ function handleSortingJoin() {
       }
       return 0;
     });
-
-    console.log(sortedData);
     tableDisplay(sortedData);
   } else {
     asc = true;
@@ -302,21 +743,18 @@ function handleSortingJoin() {
       return 0;
     });
 
-    console.log(sortedData);
     tableDisplay(sortedData);
   }
 }
-let cnt = 0;
+
 function handleDelete() {
-  console.log("delete-btn");
+  let cnt = 0;
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   for (var x of checkboxes) {
     if (x.classList.contains("checkbox-active")) {
       cnt += 1;
-      let remove_id = x.id.slice(-1);
-      console.log(remove_id);
+      let remove_id = x.id.split("-")[1];
       dataEmployess.splice(remove_id, 1);
-      console.log(dataEmployess);
     }
   }
 
@@ -369,6 +807,158 @@ function handleSearchBox() {
 }
 
 function handleFilterDept() {
+  const deptData = [
+    {
+      DEPARTMENT: "Support",
+    },
+    {
+      DEPARTMENT: "Accounting",
+    },
+    {
+      DEPARTMENT: "Business Development",
+    },
+    {
+      DEPARTMENT: "Accounting",
+    },
+    {
+      DEPARTMENT: "Research and Development",
+    },
+    {
+      DEPARTMENT: "Engineering",
+    },
+    {
+      DEPARTMENT: "Services",
+    },
+    {
+      DEPARTMENT: "Research and Development",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+    {
+      DEPARTMENT: "Engineering",
+    },
+    {
+      DEPARTMENT: "Business Development",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+    {
+      DEPARTMENT: "Human Resources",
+    },
+    {
+      DEPARTMENT: "Human Resources",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+    {
+      DEPARTMENT: "Research and Development",
+    },
+    {
+      DEPARTMENT: "Accounting",
+    },
+    {
+      DEPARTMENT: "Engineering",
+    },
+    {
+      DEPARTMENT: "Business Development",
+    },
+    {
+      DEPARTMENT: "Research and Development",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+    {
+      DEPARTMENT: "Legal",
+    },
+    {
+      DEPARTMENT: "Product Management",
+    },
+    {
+      DEPARTMENT: "Sales",
+    },
+    {
+      DEPARTMENT: "Research and Development",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+    {
+      DEPARTMENT: "Accounting",
+    },
+    {
+      DEPARTMENT: "Services",
+    },
+    {
+      DEPARTMENT: "Research and Development",
+    },
+    {
+      DEPARTMENT: "Product Management",
+    },
+    {
+      DEPARTMENT: "Engineering",
+    },
+    {
+      DEPARTMENT: "Services",
+    },
+    {
+      DEPARTMENT: "Engineering",
+    },
+    {
+      DEPARTMENT: "Product Management",
+    },
+    {
+      DEPARTMENT: "Legal",
+    },
+    {
+      DEPARTMENT: "Sales",
+    },
+    {
+      DEPARTMENT: "Business Development",
+    },
+    {
+      DEPARTMENT: "Training",
+    },
+    {
+      DEPARTMENT: "Accounting",
+    },
+    {
+      DEPARTMENT: "Business Development",
+    },
+    {
+      DEPARTMENT: "Support",
+    },
+    {
+      DEPARTMENT: "Support",
+    },
+    {
+      DEPARTMENT: "Business Development",
+    },
+    {
+      DEPARTMENT: "Product Management",
+    },
+    {
+      DEPARTMENT: "Human Resources",
+    },
+    {
+      DEPARTMENT: "Accounting",
+    },
+    {
+      DEPARTMENT: "Engineering",
+    },
+    {
+      DEPARTMENT: "Marketing",
+    },
+  ];
   let deptDropDown = "";
   deptData.map((d) => {
     deptDropDown += `<option value="${d.DEPARTMENT}">${d.DEPARTMENT}</option>`;
@@ -532,7 +1122,6 @@ function handleFilterLoc() {
   ];
   loc.map((d) => {
     locDropDown += `<option value="${d.location}">${d.location}</option>`;
-    console.log(locDropDown);
   });
   document.getElementById("filter-loc").innerHTML = locDropDown;
 }
@@ -560,7 +1149,42 @@ const handleFilterApply = () => {
 };
 
 const handleAddEmployee=()=>{
-
+document.getElementById("new-user").classList.remove("hide-new-user");
+document.getElementById("new-user").classList.add("add-new-user");
+}
+addData=(obj)=>{
+  dataEmployess.push(obj);
+  tableDisplay(dataEmployess);
 }
 tableDisplay(dataEmployess);
 alphabetsDisplay();
+
+
+document
+.getElementById("btn-submit-data")
+.addEventListener("click", () => {
+  let userName = document.getElementById("add").value;
+  let loc = document.getElementById("add-loc").value;
+  let dept = document.getElementById("add-dept").value;
+  let role = document.getElementById("add-role").value;
+  let empId = document.getElementById("add-id").value;
+  //   let status = document.getElementById("add").value;
+  let date = document.getElementById("add-date").value;
+
+  var obj = {
+    USER: userName,
+    LOCATION: loc,
+    DEPARTMENT: dept,
+    ROLE: role,
+    EMPNO: empId,
+    STATUS: true,
+    JOINDT: date,
+  };
+  addData(obj);
+});
+
+
+handleCloseAddUser=()=>{
+  document.getElementById("new-user").classList.add("hide-new-user");
+document.getElementById("new-user").classList.remove("add-new-user");
+}
